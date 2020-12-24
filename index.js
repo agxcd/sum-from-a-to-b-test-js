a181edeb7c23500b779e03a6b0404f619baba6a5
agxcd <agxcdcode@gmail.com>
2020-12-24 13:35:53 -0800

    for loop

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let sum = 0;
+  for (let i = fromN; i <= toN; i++){
+    sum += i;
+  }
+  return sum;
 }
 
 module.exports = sum;
